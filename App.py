e70bace41aa2916b94deb47565e08072d36be41a
Andy Li <qingyuanli23@mittymonarch.com>
2020-05-30 23:55:07 -0700

    mass

--- App.py
+++ App.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 import time
 import math
 from random import randint, random, uniform
+from pygame import gfxdraw
 
 
 def addVectors(v1, v2):
@@ -23,24 +24,27 @@ def find(particles, x, y):
 gravity = (math.pi, 0.1)
 drag = 0.999
 elasticity = 0.8
+mass_of_air = 0.2
 
 
 class Particle:
-    def __init__(self, pos, size, speed, angle):
+    def __init__(self, pos, size, speed, angle, mass, color):
         # x,y coordinates
         self.x = pos[0]
         self.y = pos[1]
         #size, color
         self.size = size
-        self.color = (0, 0, 0)
-        #speed, angle
+        self.color = color
+        #speed, angle, mass
         self.speed = speed
         self.angle = angle
+        self.mass = mass
+        self.drag = (self.mass/(self.mass + mass_of_air)) ** self.size
 
     def display(self, screen):
         # display on screen
-        pygame.draw.circle(screen, self.color,
-                           (self.x, self.y), self.size, 0)
+        gfxdraw.filled_circle(screen, int(self.x), int(
+            self.y), int(self.size), self.color)
 
     def move(self):
         # move according to speed and angle
@@ -49,7 +53,7 @@ class Particle:
         # move according to gravity
         self.angle, self.speed = addVectors((self.angle, self.speed), gravity)
         # add drag
-        self.speed *= drag
+        self.speed *= self.drag
 
     def bounce(self, width, height):
         if self.x <= self.size:  # left
@@ -75,17 +79,22 @@ class Particle:
         dx = self.x - p2.x
         dy = self.y - p2.y
 
-        distance = math.hypot(dx, dy)
+        distance = (dx**2 + dy**2)**0.5
         if distance < self.size + p2.size:
-            tan = math.atan2(dy, dx)
-            self.angle = 2 * tan - self.angle
-            p2.angle = 2 * tan - p2.angle
-            self.speed, p2.speed = p2.speed*elasticity, self.speed*elasticity
-            angle = 0.5 * math.pi + tan
-            self.x += math.sin(angle)
-            self.y -= math.cos(angle)
-            p2.x -= math.sin(angle)
-            p2.y += math.cos(angle)
+            angle = math.atan2(dy, dx) + 0.5 * math.pi
+            total_mass = self.mass + p2.mass
+            self.angle, self.speed = addVectors(
+                (self.angle, self.speed*(self.mass-p2.mass)/total_mass), (angle, 2*p2.speed*p2.mass/total_mass))
+            p2.angle, p2.speed = addVectors(
+                (p2.angle, p2.speed*(p2.mass-self.mass)/total_mass), (angle+math.pi, 2*self.speed*self.mass/total_mass))
+            self.speed *= elasticity
+            p2.speed *= elasticity
+
+            overlap = 0.5 * (self.size + p2.size - distance + 1)
+            self.x += math.sin(angle) * overlap
+            self.y -= math.cos(angle) * overlap
+            p2.x -= math.sin(angle) * overlap
+            p2.y += math.cos(angle) * overlap
 
 
 class Game:
@@ -93,19 +102,22 @@ class Game:
         # initialize
         pygame.init()
         pygame.display.set_caption("Physics Sim")
-        self.width = 720
-        self.height = 360
+        self.width = 1000
+        self.height = 800
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.exit = False
         self.selected_particle = None
         self.dx, self.dy = 0, 0
+        self.prevcolor = None
 
     def run(self):
         # particles
         circles = []
         for i in range(10):
+            density = randint(1, 20)
+            size = randint(20, 40)
             circles.append(
-                Particle((randint(0, self.width), randint(0, self.height)), randint(10, 20), random()*10, uniform(0, math.pi*2)))
+                Particle((randint(0, self.width), randint(0, self.height)), randint(20, 40), random()*30, uniform(0, math.pi*2), size ** 2 * density, (density * 5, density * 5, density * 5)))
 
         while not self.exit:
             for event in pygame.event.get():
@@ -119,23 +131,31 @@ class Game:
                     temp = find(
                         circles, mouseX, mouseY)
                     # if clicked particle
+
                     if self.selected_particle == temp:
+                        if self.selected_particle:
+                            self.selected_particle.color = self.prevcolor
                         self.selected_particle = None
                         self.dx, self.dy = 0, 0
                     elif temp:
+                        if self.selected_particle:
+                            self.selected_particle.color = self.prevcolor
                         self.selected_particle = temp
+                        self.prevcolor = self.selected_particle.color
                         self.selected_particle.color = (255, 0, 0)
                     else:
+                        if self.selected_particle:
+                            self.selected_particle.color = self.prevcolor
                         self.selected_particle = None
                         self.dx, self.dy = 0, 0
 
                 keys = pygame.key.get_pressed()
                 if keys[K_SPACE]:
                     for i in circles:
-                        i.speed = i.speed + random()*10
+                        i.speed = i.speed + random()*30
                 if keys[K_a]:
                     for i in circles:
-                        i.speed = i.speed + random()*10
+                        i.speed = i.speed + random()*30
                         i.angle = uniform(0, math.pi*2)
 
             # refresh screen
@@ -146,7 +166,6 @@ class Game:
                 if i != self.selected_particle:
                     i.move()
                     i.bounce(self.width, self.height)
-                    i.color = (0, 0, 0)
                     for i2 in circles[ind+1:]:
                         i.collide(i2)
                 else:
